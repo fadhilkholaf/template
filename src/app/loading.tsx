8ccd48a1d47d521b5a9ccb0e81d818e3d95b108d
Muhammad Fadhil Kholaf <fadhilgd123@gmail.com>
2025-02-12 14:41:12 +0700

    Add loading page component

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+  return (
+    <main className="flex h-full w-full items-center justify-center">
+      <p>Loading...</p>
+    </main>
+  );
+};
+
+export default LoadingPage;
